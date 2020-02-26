e6568bd3e1b7927cc500e9da6433eb6567893781
shiswq@hletong.com <shiswq@hletong.com>
2020-02-26 09:15:39 +0800

    fix: 🐛 引入icons,减少报错

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,10 +5,24 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
-import { NZ_I18N, zh_CN } from 'ng-zorro-antd';
+
 import { ServicesModule } from '../services/services.module';
 import { PagesModule } from '../pages/pages.module';
 import { ShareModule } from '../share/share.module';
+
+
+// 如何引入icon减少报错   https://ng.ant.design/components/icon/zh
+import { IconDefinition } from '@ant-design/icons-angular';
+import { NzIconModule, NZ_ICON_DEFAULT_TWOTONE_COLOR, NZ_ICONS } from 'ng-zorro-antd/icon';
+import { NZ_I18N, zh_CN } from 'ng-zorro-antd';
+
+// 引入全部的图标，不推荐 ❌
+import * as AllIcons from '@ant-design/icons-angular/icons';
+const antDesignIcons = AllIcons as {
+[key: string]: IconDefinition;
+};
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key]);
+
 registerLocaleData(zh);
 
 /***CoreModule相当于AppModule的助手,极大的减轻AppModule的负担,并且只能被AppModule注入****/
@@ -21,13 +35,18 @@ registerLocaleData(zh);
     ServicesModule,
     PagesModule,
     ShareModule,
+    NzIconModule,
     AppRoutingModule
   ],
   exports: [
     ShareModule,
     AppRoutingModule
   ],
-  providers: [{ provide: NZ_I18N, useValue: zh_CN }],
+  providers: [
+    { provide: NZ_I18N, useValue: zh_CN },
+    { provide: NZ_ICON_DEFAULT_TWOTONE_COLOR, useValue: '#00ff00' }, // 不提供的话，即为 Ant Design 的主题蓝色
+    { provide: NZ_ICONS, useValue: icons }
+  ],
 })
 export class CoreModule {                                            // https://angular.cn/api/core/Optional
   constructor(@SkipSelf() @Optional()   parentModule: CoreModule) { // https://angular.cn/api/core/SkipSelf
