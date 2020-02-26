72e27938bd407ec624fa7c598052c345308e0032
shiswq@hletong.com <shiswq@hletong.com>
2020-02-26 13:40:57 +0800

    feat: 🎸 home组件打印响应结果

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { ShareModule } from 'src/app/share/share.module';
+import { HomeRoutingModule } from './home-routing.module';
+import { HomeComponent } from './home.component';
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [
+    ShareModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
