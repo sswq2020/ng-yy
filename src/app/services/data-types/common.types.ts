26cf38ff0db261bc450fb50fdfd854df190b186e
shiswq@hletong.com <shiswq@hletong.com>
2020-02-27 10:32:19 +0800

    feat: 🎸 热门标签,歌单接口服务

--- src/app/services/data-types/common.types.ts
+++ src/app/services/data-types/common.types.ts
@@ -1,5 +1,21 @@
+/***轮播图接口**/
 export interface Banner {
   targetId: number;
   url: string | null;
   imageUrl: string;
+}
+
+/***热门标签接口**/
+export interface HotTag {
+  id: number;
+  name: string | null;
+  position: number;
+}
+
+/***歌单接口**/
+export interface SongSheet {
+  id: number;
+  name: string | null;
+  picUrl: string;
+  playCount: number;
 }
--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject, Inject } from '@angular/core';
 import { ServicesModule, API_CONFIG_BASE_URL } from './services.module';
 import { Observable } from 'rxjs';
-import { Banner } from './data-types/common.types';
+import { Banner, HotTag, SongSheet } from './data-types/common.types';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/internal/operators';
 @Injectable({
@@ -16,4 +16,17 @@ export class HomeService {
       map((res: { banners: Banner[] }) => res.banners)
     );
   }
+
+  getHotTags(): Observable<HotTag[]> {
+    return this.http.get(`${this.uri}playlist/hot`).pipe(
+      map((res: { tags: HotTag[] }) => res.tags)
+    );
+  }
+
+  getPersonalSheetList(): Observable<SongSheet[]> {
+    return this.http.get(`${this.uri}personalized`).pipe(
+      map((res: { result: SongSheet[] }) => res.result)
+    );
+  }
+
 }
