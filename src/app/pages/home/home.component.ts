81580a7e58d3eeb8501c31eccecbf3e35710e357
shiswq@hletong.com <shiswq@hletong.com>
2020-02-27 09:37:19 +0800

    feat: 🎸 自定义封装轮播图的组件,实现点击左右也能切换,小小的组件封装学到很多
    ng-content和vue里的slot工能相似,模板变量在视图和模型获取方式不一样,@ViewChild既能获取模板,也能获取组件,let-变量的用法,父子组件传值的关系,changeDection策略模式

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
 import { HomeService } from 'src/app/services/home.service';
 import { Banner } from 'src/app/services/data-types/common.types';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { NzCarouselComponent } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-home',
@@ -8,16 +10,27 @@ import { Banner } from 'src/app/services/data-types/common.types';
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
-
+  carouselActiveIndex = 0;
   banners: Banner[];
 
+  @ViewChild('wyCarousel', { static: true }) wyCarousel: TemplateRef<any>;
+  // 使用ViewChild获取子组件的实例 https://www.jianshu.com/p/ac5366abfa74
+  @ViewChild(NzCarouselComponent, {static: true})  private nzcarousel: NzCarouselComponent;
   constructor(private homeService: HomeService) {
     this.homeService.getBanners().subscribe((banners) => {
       this.banners = banners;
-    })
+    });
   }
 
   ngOnInit(): void {
+    console.info((this.wyCarousel));
   }
 
+  onBeforeChange({ to }) {
+    this.carouselActiveIndex = to;
+  }
+
+  getType(type: string) {
+    this.nzcarousel[type]();
+  }
 }
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { ShareModule } from 'src/app/share/share.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { WyCarouselComponent } from './components/wy-carousel/wy-carousel.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, WyCarouselComponent],
   imports: [
     ShareModule,
     HomeRoutingModule
