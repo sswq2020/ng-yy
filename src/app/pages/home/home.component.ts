7d89dcde1e528573ac35574132472af551fe9b98
shiswq@hletong.com <shiswq@hletong.com>
2020-02-28 15:35:44 +0800

    feat: 🎸 对热门标签和歌单数组进行切割

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild, TemplateRef } from '@angular/core';
 import { HomeService } from 'src/app/services/home.service';
-import { Banner } from 'src/app/services/data-types/common.types';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Banner, HotTag, SongSheet } from 'src/app/services/data-types/common.types';
 import { NzCarouselComponent } from 'ng-zorro-antd';
 
 @Component({
@@ -12,13 +11,35 @@ import { NzCarouselComponent } from 'ng-zorro-antd';
 export class HomeComponent implements OnInit {
   carouselActiveIndex = 0;
   banners: Banner[];
+  hotTags: HotTag[];
+  songSheetList: SongSheet[];
 
   @ViewChild('wyCarousel', { static: true }) wyCarousel: TemplateRef<any>;
   // 使用ViewChild获取子组件的实例 https://www.jianshu.com/p/ac5366abfa74
   @ViewChild(NzCarouselComponent, {static: true})  private nzcarousel: NzCarouselComponent;
   constructor(private homeService: HomeService) {
+    this.getBanners();
+    this.getHotTags();
+    this.getPersonalSheetList();
+  }
+
+  private getBanners() {
     this.homeService.getBanners().subscribe((banners) => {
       this.banners = banners;
+    });
+  }
+
+  private getHotTags() {
+    this.homeService.getHotTags().subscribe((hotTags) => {
+      this.hotTags = hotTags.sort((a, b) => {
+        return a.position - b.position;
+      }).slice(0, 5);
+    });
+  }
+
+  private getPersonalSheetList() {
+    this.homeService.getPersonalSheetList().subscribe((songSheet) => {
+      this.songSheetList = songSheet.slice(0, 16);
     });
   }
 
