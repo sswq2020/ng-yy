88727cf5398b2341a244ae6dbcf739fe1c28423a
shiswq@hletong.com <shiswq@hletong.com>
2020-02-29 11:15:54 +0800

    feat: 🎸 首页登陆组件简单封装一下

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,9 +3,10 @@ import { ShareModule } from 'src/app/share/share.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { WyCarouselComponent } from './components/wy-carousel/wy-carousel.component';
+import { MemberCardComponent } from './components/member-card/member-card.component';
 
 @NgModule({
-  declarations: [HomeComponent, WyCarouselComponent],
+  declarations: [HomeComponent, WyCarouselComponent, MemberCardComponent],
   imports: [
     ShareModule,
     HomeRoutingModule
