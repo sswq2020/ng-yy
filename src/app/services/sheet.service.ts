64aa8eed33dc753a45e1a32a606fec4ce819ede5
sswq2020 <a309108243@qq.com>
2020-03-02 17:14:12 +0800

    feat: 🎸 主要要做了获取歌单里每首歌的数据,但数据结构里缺播放地址,所以需要再请求别的服务来完善数据结构

--- src/app/services/sheet.service.ts
+++ src/app/services/sheet.service.ts
@@ -1,9 +1,10 @@
 import { Injectable, inject, Inject } from '@angular/core';
 import { ServicesModule, API_CONFIG_BASE_URL } from './services.module';
 import { Observable } from 'rxjs';
-import { SongSheet } from './data-types/common.types';
+import { SongSheet, Song } from './data-types/common.types';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { map } from 'rxjs/internal/operators';
+import { map, pluck, switchMap } from 'rxjs/internal/operators';
+import { SongService } from './song.service';
 
 
 @Injectable({
@@ -11,7 +12,7 @@ import { map } from 'rxjs/internal/operators';
 })
 export class SheetService {
 
-  constructor(private http: HttpClient, @Inject(API_CONFIG_BASE_URL) private uri: string) { }
+  constructor(private http: HttpClient, private songServe: SongService, @Inject(API_CONFIG_BASE_URL) private uri: string) { }
 
   getSongSheetDetail(id: number): Observable<SongSheet> {
     // 注意get请求参数的处理
@@ -21,6 +22,21 @@ export class SheetService {
     return this.http.get(`${this.uri}playlist/detail`, {params}).pipe(
       map((res: { playlist: SongSheet}) => res.playlist)
     );
+  }
+
+  /***
+   * 1.这里需要包含播放地址歌曲数组,处理逻辑有Songserve去做
+   * 2.这种一个请求依赖上个请求结果的时候,请用switchMap
+   * 3.你可以这么理解,之前请求都是map操作,现在切换到另一个map上
+   * **/
+  playSheet(id: number): Observable<Song[]> {
+     return this.getSongSheetDetail(id).pipe(
+       // plick操作符 https://www.bookstack.cn/read/Learn-RxJS-zh/operators-transformation-pluck.md
+       pluck('tracks'),
+       switchMap(tracks => {
+         return this.songServe.getSongList(tracks);
+       })
+     );
   }
 
 }
--- src/app/services/song.service.ts
+++ src/app/services/song.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, inject, Inject } from '@angular/core';
 import { ServicesModule, API_CONFIG_BASE_URL } from './services.module';
-import { Observable } from 'rxjs';
-import { SongUrl } from './data-types/common.types';
+import { Observable, observable } from 'rxjs';
+import { SongUrl, Song } from './data-types/common.types';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { map } from 'rxjs/internal/operators';
 
@@ -13,14 +13,44 @@ export class SongService {
 
   constructor(private http: HttpClient, @Inject(API_CONFIG_BASE_URL) private uri: string) { }
 
+  /***根据ids字符串返回一个或者多个歌曲播放地址**/
   getSongUrl(ids: string): Observable<SongUrl[]> {
-    // 注意get请求参数的处理
-    // 1.通过 new HttpParams()
-    // 2.单个参数可以set方法来书写
     const params = new HttpParams().set('id', ids);
-    return this.http.get(`${this.uri}song/url`, {params}).pipe(
-      map((res: { data: SongUrl[]}) => res.data)
+    return this.http.get(`${this.uri}song/url`, { params }).pipe(
+      map((res: { data: SongUrl[] }) => res.data)
     );
   }
+
+  /***
+   * 1.歌单数组里已经包含每首歌的数据,但是缺播放url;
+   * 2.因此根据id获取url,之后还要拼接;
+   * 3.返回的数据必须是个Observable,因此需要 Observable.create和next方法
+  **/
+  getSongList(songs: Song | Song[]): Observable<Song[]> {
+    const that = this;
+    const songArr = Array.isArray(songs) ? songs.slice() : [songs];
+    const ids = songArr.map(song => song.id).join(',');
+    return Observable.create(observer => {
+      that.getSongUrl(ids).subscribe(urls => {
+        observer.next(that.generateSongList(songArr, urls));
+       });
+    });
+  }
+
+  /**
+   * @description 这个方法就是为了将歌单里每首歌曲都拼接上播放地址,必经是2个接口
+   */
+  private generateSongList(songs: Song[], urls: SongUrl[]): Song[] {
+    const list = [];
+    songs.forEach(item => {
+      const index = urls.findIndex(url => url.id === item.id);
+      if (index > -1) {
+        list.push({ ...item, url: urls[index].url });
+      }
+    });
+    return list;
+  }
+
+
 
 }
