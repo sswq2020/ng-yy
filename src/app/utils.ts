2a952ecca9558b0ef7c91776998f8cf88ef3d57c
shiswq@hletong.com <shiswq@hletong.com>
2020-03-04 14:07:24 +0800

    feat: 🎸 新增限制范围的函数

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,3 +1,9 @@
+/***数组是否包含某项**/
 export function inArr(arr: any[], target: any): boolean {
   return arr.indexOf(target) > -1;
+}
+
+/***限制数的范围**/
+export function limitNumberInRange(val: number, min: number, max: number): number {
+  return Math.min(Math.max(val, min), max);
 }
