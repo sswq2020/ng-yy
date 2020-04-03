8ee4c7aa3d57744e77cad6e37fd462e06bb105b5
shiswq@hletong.com <shiswq@hletong.com>
2020-04-03 21:36:56 +0800

    fix: 🐛 修复如果先改变播放模式为随机,再点击播放列表也能正确播放模式

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,8 +5,11 @@ import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs/internal/operators';
 import { SheetService } from 'src/app/services/sheet.service';
 import { AppStoreModule } from 'src/app/store';
-import { Store } from '@ngrx/store';
+import { Store, select } from '@ngrx/store';
 import { setSongList, setPlayList, setCurrentIndex } from 'src/app/store/actions/player.actions';
+import { getPlayer, getPlayMode } from 'src/app/store/selectors/player.selector';
+import { PlayMode } from 'src/app/share/wy-ui/wy-player/player-types';
+import { shuffle, _findIndex } from 'src/app/utils';
 
 @Component({
   selector: 'app-home',
@@ -19,6 +22,8 @@ export class HomeComponent implements OnInit {
   hotTags: HotTag[];
   songSheetList: SongSheet[];
   singers: Singer[];
+  /***播放模式**/
+  mode: PlayMode;
 
   @ViewChild('wyCarousel', { static: true }) wyCarousel: TemplateRef<any>;
   // 使用ViewChild获取子组件的实例 https://www.jianshu.com/p/ac5366abfa74
@@ -32,6 +37,11 @@ export class HomeComponent implements OnInit {
       this.hotTags = hotTags;
       this.songSheetList = songSheetList;
       this.singers = singers;
+    });
+
+    const appStore$ = this.store$.pipe(select(getPlayer));
+    appStore$.pipe(select(getPlayMode)).subscribe(mode => {
+      this.mode = mode;
     });
   }
 
@@ -50,8 +60,14 @@ export class HomeComponent implements OnInit {
   onPlaySheet(id: number) {
     this.sheetServe.playSheet(id).subscribe(list => {
       this.store$.dispatch(setSongList({songList: list}));
+      let playIndex = 0;
+      const playSong = list[playIndex];
+      if (this.mode.type === 'random') {
+        list = shuffle(list);
+        playIndex = _findIndex(list, playSong);
+      }
       this.store$.dispatch(setPlayList({playList: list}));
-      this.store$.dispatch(setCurrentIndex({currentIndex: 0}));
+      this.store$.dispatch(setCurrentIndex({currentIndex: playIndex}));
     });
   }
 }
